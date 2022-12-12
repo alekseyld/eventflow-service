b2e2d3c1872017c331dfd4d0ada51b686493299f
alekseyld <al.lisow1@gmail.com>
2022-12-12 22:25:27 +0300

    init backend

---
+++ backend/src/main/java/com/alekseyld/eventflowbackend/EventflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alekseyld.eventflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alekseyld/eventflowbackend/EventflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseyld.eventflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
