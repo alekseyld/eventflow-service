63eab04e4df449218042a41354785865aff971e1
alekseyld <al.lisow1@gmail.com>
2022-12-13 20:26:32 +0300

    add workflow for backend

---
+++ backend/src/test/java/com/alekseyld/eventflowbackend/SimpleUnitTest.java
@@ -0,0 +1,13 @@
+package com.alekseyld.eventflowbackend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleUnitTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
