4b70a8ff1327ba8ff72cab599c81cb47d30f2f00
alekseyld <al.lisow1@gmail.com>
2023-01-07 19:19:28 +0300

    [Frontend] add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
+import {TimelineCustom} from "./features/timeline/TimelineCustom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*<Counter />*/}
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<Counter />*/}
 
+                <TimelineCustom/>
 
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/features/timeline/TimelineCustom.tsx
@@ -0,0 +1,15 @@
+import {Button, Timeline} from "flowbite-react";
+import React from "react";
+
+export function TimelineCustom() {
+
+    return (
+        <g className="event-node-icon">
+            <circle fill="#1A73E8" cx="12" cy="12" r="12"></circle>
+            <path fill="#FFFFFF" d="M17,13.3l-3.3-1.7c-0.2,0-0.3-0.2-0.4-0.2h-0.6V7.3c0-0.6-0.4-1-1-1s-1.1,0.5-1.1,1v7.8L8,14.7H7.8
+      c-0.3,0-0.5,0.2-0.6,0.3l-0.6,0.6l3.6,3.6c0.2,0.1,0.4,0.3,0.8,0.3h4.9c0.6,0,0.9-0.4,1-0.9l0.6-3.9v-0.2C17.5,14,17.2,13.7,17,13.3
+      z M9.8,10V7.3c0-1,0.8-1.8,1.8-1.8s1.8,0.8,1.8,1.8V10c0.9-0.6,1.5-1.6,1.5-2.7c0-1.8-1.5-3.3-3.3-3.3S8.2,5.5,8.2,7.3
+      C8.2,8.4,8.8,9.5,9.8,10z"></path>
+        </g>
+    )
+}
